8b271cb77fa1ed2874ae6550dfdfceb8cb91db27
hanbowen <hanbowen@lucigo.com>
2022-06-17 20:50:05 +0800

    rename short argument libbluray to bluray delete unused variables

--- scripts/libbluray/build.sh
+++ scripts/libbluray/build.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-LIBLURAY_ADDITIONAL_FLAGS=
-
 CC=${FAM_CC} \
 AR=${FAM_AR} \
-AS=${X264_AS} \
+AS=${FAM_AS} \
 RANLIB=${FAM_RANLIB} \
 STRIP=${FAM_STRIP} \
 LIBS="-lz" \
@@ -17,8 +15,7 @@ JDK_HOME="/usr/lib/jvm/adoptopenjdk-8-hotspot-amd64" \
     --with-pic \
     --without-libxml2 \
     --without-fontconfig \
-    --disable-bdjava-jar \
-    ${LIBLURAY_ADDITIONAL_FLAGS} || exit 1
+    --disable-bdjava-jar || exit 1
 
 ${MAKE_EXECUTABLE} clean
 ${MAKE_EXECUTABLE} -j${HOST_NPROC}
